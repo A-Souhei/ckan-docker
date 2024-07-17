04cc9bacd38084153d818b32e885fb1ff0263412
devx <toavinahajaina@gmail.com>
2024-07-17 19:29:19 +0300

    Update debug.sh docker commands
    This commit updates the docker commands in debug.sh to ensure the ckan_docker-ckan container is removed before the ckan_app_dir volume. This adjustment ensures a clean teardown and build scenario for the CKAN docker environment.

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose down ckan_docker-ckan && docker volume rm ckan_app_dir
+docker compose down ckan_docker-ckan && docker rm ckan_docker-ckan && docker volume rm ckan_app_dir
 docker compose build ckan && docker compose up -d ckan
 
 
